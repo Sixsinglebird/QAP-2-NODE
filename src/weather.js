7875387153a464fbaaf844e2642b7ebd49b4203f
Alex Singleton <alex.singleton@keyin.com>
2023-05-27 00:15:58 -0230

    init

--- src/weather.js
+++ src/weather.js
@@ -8,7 +8,10 @@ const conditions = async (res) => {
     const response = await fetch(weatherUrl);
     const data = await response.text();
     await res.writeHead(res.statusCode, { "Content-Type": "text/html" });
-    await res.end(data);
+    await res.end(
+      "<head><title>NFLD Weather</title><style>* {margin: 0;padding: 0;}header {display: flex;max-width: 100%;}header nav {margin-left: auto;}</style></head><body><header><h1>Sixsinglebird</body></header></h1>" +
+        data
+    );
   } catch (error) {
     res.statusCode = 500;
     console.error(error.message);
