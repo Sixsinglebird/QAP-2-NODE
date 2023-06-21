021bc7cabbd30316371121684e4f50952d5614b8
Alex Singleton <alex.singleton@keyin.com>
2023-06-21 17:22:47 -0230

    Update App.js

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
 ////////////////////////////////////////////////
 // globals
 global.DEBUG = true;
-global.STYLE = '<link rel="stylesheet" href="files/style.css" ></link>';
+global.STYLE = '<link rel="stylesheet" href="files/style.css" />';
 global.NAV =
   '<nav><a href="/subscribe">subscribe</a><a href="/products">products</a><a href="/contact">contact</a><a href="/about">about</a>  <a href="/weather">weather</a><nav><a href="/">home</a></nav>';
 
@@ -21,3 +21,4 @@ const server = require("./src/server");
 server.serverSwitch.listen(3000, "localhost", () => {
   if (global.DEBUG) console.log("Listening on port 3000...");
 });
+``;
