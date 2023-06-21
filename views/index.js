d0ed56ead3f551be6e21825bcf3dcca07c00ae23
Alex Singleton <alex.singleton@keyin.com>
2023-06-21 17:00:19 -0230

    oops

--- views/index.js
+++ views/index.js
@@ -1,8 +1,7 @@
 ////////////////////////////////////////////////
 const page = async (res) => {
   const name = "Landing Page";
-  const style = `${global.STYLE}`;
-  const head = `<head>${style}<title>${name}</title></head>`;
+  const head = `<head>${global.STYLE}<title>${name}</title></head>`;
   const header = `<header><h1>${name}</h1>${global.NAV}</header>`;
   try {
     await res.writeHead(res.statusCode, { "Content-Type": "text/html" });
